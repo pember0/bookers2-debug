e86deece5bd8e0c64a37c8d1624b285cd909bea4
pember0 <134245409+pember0@users.noreply.github.com>
2023-07-08 10:50:04 +0000

    課題6【課題3で実装した機能の非同期通信化】_20230708

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
   has_many :favorites, dependent: :destroy      # いいね機能
-  has_many :book_comment, dependent: :destroy   # コメント機能
+  has_many :book_comments, dependent: :destroy   # コメント機能
 
   # 引数で渡されたユーザidがFavoritesテーブル内に存在（exists?）するかどうかを調べます。
   def favorited_by?(user)
